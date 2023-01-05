6e3092995643c383cc15373dce176fbb46b1874c
HyungJun <9h0jun1115@gmail.com>
2023-01-05 10:48:37 +0900

    Create coin_change.py

---
+++ week2/hyungjun/coin_change.py
@@ -0,0 +1,16 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if not amount:
+            return 0
+        x = [0] * (amount+1)
+        for coin in coins:
+            if coin <= amount:
+                x[coin] = 1
+        for i in range(1, amount+1):
+            l=[x[i-c] + x[c] for c in coins if (i>c and x[i-c] !=0) or (i==c and x[i-c] == 0)]
+            if not len(l):
+                l += [0]
+            x[i] = min(l)
+            if x[i] == 1 and i not in coins:
+                x[i] = 0
+        return x[amount] if x[amount] else -1
