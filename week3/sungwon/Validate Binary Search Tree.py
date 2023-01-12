19e2dd5f9c5bd1a99ea0b24f4feb940e5bc9deaf
welshimeat <57587295+welshimeat@users.noreply.github.com>
2023-01-13 00:42:40 +0900

    Create Validate Binary Search Tree.py

---
+++ week3/sungwon/Validate Binary Search Tree.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        if root is None:
+            return True
+        return self.validateBST(root, -2**31-1, 2**31+1)
+
+    def validateBST(self, node, min, max):
+        if node.left is not None:
+            if node.left.val >= node.val:
+                return False
+            if node.left.val <= min:
+                return False
+            if not self.validateBST(node.left, min, node.val):
+                return False
+            
+        if node.right is not None:
+            if node.right.val <= node.val:
+                return False
+            if node.right.val >= max:
+                return False
+            if not self.validateBST(node.right, node.val, max):
+                return False
+        
+        return True
