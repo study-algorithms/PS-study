589cfe0c88594513c52db878b2a98881f49c516a
Danielle Daeun Lee <77555889+danielledaeun@users.noreply.github.com>
2023-02-10 10:28:08 +0900

    Create rotate_sorted.py

---
+++ week6/Danielle/rotate_sorted.py
@@ -0,0 +1,21 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start, end = 0, len(nums) - 1
+    
+        while start <= end:
+            mid = start + (end-start) // 2
+            
+            if nums[mid] == target:
+                return mid
+            
+            elif nums[mid] >= nums[start]:
+                if target <= nums[mid] and target >=nums[start]:
+                    end = mid - 1
+                else:
+                    start = mid + 1
+            else:
+                if target >= nums[mid] and target <= nums[end]:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+        return -1   
