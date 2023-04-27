8e4355c6c4e73075ca0e7492777f578be2469d69
welshimeat <57587295+welshimeat@users.noreply.github.com>
2023-04-28 01:25:36 +0900

    Delete 연속 펄스 부분 수열의 합.py

--- week13/연속 펄스 부분 수열의 합.py
+++
@@ -1,9 +0,0 @@
-def solution(sequence):
-    maxSum1, maxSum2 = [0] * (len(sequence)+1), [0] * (len(sequence)+1)
-    turn = 1
-    for i, v in enumerate(sequence):
-        maxSum1[i+1] = max(maxSum1[i] + v*turn, v*turn)
-        maxSum2[i+1] = max(maxSum2[i] - v*turn, -1*v*turn)
-        turn = -1 * turn
-    answer = max(max(maxSum1), max(maxSum2))
-    return answer
