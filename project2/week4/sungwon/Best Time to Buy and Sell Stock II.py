1417f715b21060f58a0e4f7e3fdfe70d17f29703
welshimeat <57587295+welshimeat@users.noreply.github.com>
2023-08-11 10:42:42 +0900

    Create Best Time to Buy and Sell Stock II.py

---
+++ project2/week4/sungwon/Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy = prices[0]
+        profit = 0 
+        for i in range(1, len(prices)):
+            if prices[i] > buy:
+                profit += prices[i] - buy
+            buy = prices[i]
+        return profit
